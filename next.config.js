6c07963f37ff8ddc6d3ea1cfc43b12c51266bf04
Patrick Collings <patrickcollingss@gmail.com>
2021-10-03 23:01:15 +0100

    config change

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
-const env = process.env.ENV || "development";
-
-const configs = {
-  development: {
-    API_URL: "http://localhost:8080/graphql",
-  },
-  production: {
-    API_URL: "http://phishme-env.eba-k6jppnz9.eu-west-2.elasticbeanstalk.com",
-  },
-}[env];
-
 module.exports = {
-  env: {
-    API_URL: configs.API_URL,
+  publicRuntimeConfig: {
+    API_URL: proccess.env.API_URL,
   },
 };
