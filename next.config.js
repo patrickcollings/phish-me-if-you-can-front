3965c7a13463b842dee8f091438f5d20c433e7cc
Patrick Collings <patrickcollingss@gmail.com>
2021-10-03 22:51:32 +0100

    add prd next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
+const env = process.env.ENV || "development";
+
+const configs = {
+  development: {
+    API_URL: "http://localhost:8080/graphql",
+  },
+  production: {
+    API_URL: "http://phishme-env.eba-k6jppnz9.eu-west-2.elasticbeanstalk.com",
+  },
+}[env];
+
 module.exports = {
   env: {
-    API_URL: "http://localhost:8080/graphql",
+    API_URL: configs.API_URL,
   },
 };
