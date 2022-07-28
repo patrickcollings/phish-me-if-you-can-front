e750a225da784e1765d2100944a91041a5952d76
Patrick Collings <patrickcollingss@gmail.com>
2022-07-28 22:37:37 +0100

    add security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
 const path = require('path')
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src www.phishmeifyoucan.com;
+  style-src 'self' www.phishmeifyoucan.com;
+  font-src 'self';  
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+];
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
@@ -9,6 +32,15 @@ module.exports = {
     API_URL: process.env.API_URL,
     GAME_URL: process.env.GAME_URL,
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
