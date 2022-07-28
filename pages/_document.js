034583aadc6fc466340f95d87ee46b1375122132
Patrick Collings <patrickcollingss@gmail.com>
2022-07-28 22:54:21 +0100

    change lang to en-gb

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
 
     render() {
         return (
-          <Html lang="en-uk">
+          <Html lang="en-gb">
             <Head>
               <link
                 href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i&display=swap"
